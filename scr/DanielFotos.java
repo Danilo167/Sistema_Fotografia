3b2de221cfb0db0b2ee57ce305580a64c9dad84b
Schei/εγώ <dscr9669@gmail.com>
2026-03-06 11:45:01 -0300

    Update DanielFotos.java
    Danilo

--- scr/DanielFotos.java
+++ scr/DanielFotos.java
@@ -2,5 +2,6 @@ public class DanielFotos {
     public static void main(String[] args) {
         System.out.println("Daniel é mto zika");
         System.out.println("Renato esteve aqui");
+        System.out.println("Eu Danilo!");
     }
 }
