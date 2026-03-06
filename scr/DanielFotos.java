cdde5ceb276815f5957a47cf67f2cb2deabc0a77
renatofelixsn <emailsrenato@gmail.com>
2026-03-06 11:41:11 -0300

    Update DanielFotos.java

--- scr/DanielFotos.java
+++ scr/DanielFotos.java
@@ -1,5 +1,6 @@
 public class DanielFotos {
     public static void main(String[] args) {
         System.out.println("Daniel é mto zika");
+        System.out.println("Renato esteve aqui");
     }
 }
